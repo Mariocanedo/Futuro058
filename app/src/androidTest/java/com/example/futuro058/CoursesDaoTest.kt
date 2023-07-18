1364a15ed673850f0b607057371a0014c98c2124
StudioOne <Mario_cane.do@hotmail.com>
2023-07-18 10:34:07 -0400

    Implementatio Daotest

--- app/src/androidTest/java/com/example/futuro058/CoursesDaoTest.kt
+++ app/src/androidTest/java/com/example/futuro058/CoursesDaoTest.kt
@@ -3,16 +3,15 @@ package com.example.futuro058
 
 import android.content.Context
 import android.os.Build.VERSION_CODES.Q
+
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.futuro058.Modelo.CentroFuturoRepository
 import com.example.futuro058.Modelo.local.CentroFuturoDao
 import com.example.futuro058.Modelo.local.database.CoursesDataBase
 import com.example.futuro058.Modelo.local.entities.CoursesDetailEntity
 import com.example.futuro058.Modelo.local.entities.CoursesEntity
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
@@ -20,12 +19,11 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
-import org.robolectric.annotation.Config
+
 
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk=[Q], manifest = Config.NONE)
+//@Config(sdk=[Q], manifest = Config.NONE)
 class CoursesDaoTest {
 
 
@@ -33,23 +31,23 @@ class CoursesDaoTest {
     private lateinit var coursesDaoTest : CentroFuturoDao
     private lateinit var db: CoursesDataBase
 
-    private lateinit var  coursesRepositoryMock: CentroFuturoRepository
-    @Before
-        fun setUp2(){
-
-        val context= ApplicationProvider.getApplicationContext<Context>()
-        db= Room.inMemoryDatabaseBuilder(context,CoursesDataBase::class.java).build()
-        coursesDaoTest= db.getCentroFuturoDao()
-        // inicializar el mock
-        coursesRepositoryMock = Mockito.mock(CentroFuturoRepository::class.java)
-
-//        Mockito.`when`( coursesRepositoryMock.coursesListLiveData()).thenReturn(listOf(
-//            CoursesEntity("43", "Prueba1","test1","url",4,"march"),
-//            CoursesEntity("44", "Prueba2","test2","url",4,"march"),
-
-      //  ))
-
-    }
+ //   private lateinit var  coursesRepositoryMock: CentroFuturoRepository
+//    @Before
+//        fun setUp2(){
+//
+//        val context= ApplicationProvider.getApplicationContext<Context>()
+//        db= Room.inMemoryDatabaseBuilder(context,CoursesDataBase::class.java).build()
+//        coursesDaoTest= db.getCentroFuturoDao()
+//        // inicializar el mock
+//        coursesRepositoryMock = Mockito.mock(CentroFuturoRepository::class.java)
+//
+////        Mockito.`when`( coursesRepositoryMock.coursesListLiveData()).thenReturn(listOf(
+////            CoursesEntity("43", "Prueba1","test1","url",4,"march"),
+////            CoursesEntity("44", "Prueba2","test2","url",4,"march"),
+//
+//      //  ))
+//
+//    }
 
 
 
